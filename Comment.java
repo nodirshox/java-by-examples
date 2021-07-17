e4131fc8789463739dfebee8be316716e40a915b
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2021-07-17 20:23:18 +0500

    add Comments

---
+++ Comment.java
@@ -0,0 +1,11 @@
+// single-line comment
+
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("Comment example"); // you can add single-line comment
+    }
+}
+/*
+	Multi-line comment example.
+	Write anything here.
+*/
